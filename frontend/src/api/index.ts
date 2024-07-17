926fa51c81aa164c922c9965dd7b8d679c093ba0
namnm204 <nammmmmm008@gmail.com>
2024-07-17 17:59:41 +0700

    sfef

--- frontend/src/api/index.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const instace = axios.create({
-  baseURL: "http://localhost:3000",
-  headers: { "Content-Type": "application/json" },
-  timeout: 3000,
-});
--- frontend/src/component/PriveVateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PriveVateRoute = () => {
-  const acccessToken = JSON.parse(localStorage.getItem("user"))?.accessToken;
-  return acccessToken ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PriveVateRoute;
